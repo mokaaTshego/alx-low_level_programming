884e6dbe5fcbcfcea75e0436e2849d77dfddc31c
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-03 05:19:10 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  *  *main - entry point
  *   *
- *    *Return: 0 after printing the function    
+ *    *Return: 0 after printing the function
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
