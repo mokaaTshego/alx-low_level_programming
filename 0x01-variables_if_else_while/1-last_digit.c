aa06321875e999ff74d8edf47185c2975915ab3b
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-04 07:36:04 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,12 +15,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 lastdigit = n % 10;
-if ( lastdigit == 0 )
+if (lastdigit == 0)
 {
-	printf("Last digit of %i is %i and is zero\n", n, lastdigit);
+	printf("Last digit of %i is %i and is 0\n", n, lastdigit);
 }
 
-else if ( lastdigit > 5 )
+else if (lastdigit > 5)
 {
 	printf("Last digit of %i is %i and is greater than 5\n", n, lastdigit);
 }
@@ -30,5 +30,5 @@ else
 	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastdigit);
 }
 
-return(0);
+return (0);
 }
