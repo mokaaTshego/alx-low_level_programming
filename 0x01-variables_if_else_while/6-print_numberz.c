9ed12bf96b09543e77219ca1dcf2aaa189bd4516
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-04 12:58:39 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar((char)i);
+	}
+	putchar('\n');
+
+	return (0);
+}
