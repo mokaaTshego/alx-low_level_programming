b88e8d1b50e06e314b0a7affe18493b5327a52b8
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-06 15:54:09 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  * jack_bauer -prints every minute of the day
+ *   *
+ *    * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int i, n;
+
+	for (i = 0; i < 24; i++)
+	{
+		for(n = 0; n <= 59; n++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
