70c99dfed7628bb19ee95d8436c3fc53f75f5bef
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-06 16:09:58 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * add - adds two integers and returns the result
+ *  *@a and @b : uses to find sum
+ *  * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
