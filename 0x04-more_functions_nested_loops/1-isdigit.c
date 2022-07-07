e6b3e70185b2efdc58f0164a81e10b4b16853d2c
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-07 12:44:22 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character                                                                                    
+ *  @c: carrier integer variable                                                                                                 
+ *  Return: Always 0.                                                                                                            
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
