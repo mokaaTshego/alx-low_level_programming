3d39fd4c75d0dfc00e5746d3ecf8f9be843f6553
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-08 12:41:03 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,14 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 1; j <= 14; ++j)
+		for (j = 0; j <= 14; ++j)
 		{
 			a = j;
-			if(j > 9)
+			if (j > 9)
 			{
 				_putchar('1');
 				a = j % 10;
 			}
-			
 			_putchar('0' + a);
 		}
 		_putchar('\n');
