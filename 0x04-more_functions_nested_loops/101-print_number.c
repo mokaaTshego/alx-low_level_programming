7ae66f12764025c9ca5433469213975ccd1d2bd5
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-08 15:43:41 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n : an integer to print
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -n;
+	}
+	if (a / 10)
+		print_number(a / 10);
+	_putchar((a % 10) + '0');
+}
