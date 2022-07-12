31c77cbade5e4032d2bb109c583b135520c1d7f6
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-12 12:52:56 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,14 @@ void puts_half(char *str)
 	if (len % 3 == 0)
 	{
 		len1 = (len - 1) / 2;
-		for (i = len1 + 1; i <= len; i++)
+		for (i = len1 - 1; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (len / 2) - 1; i <= len; i++)
+		for (i = (len / 2); i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
