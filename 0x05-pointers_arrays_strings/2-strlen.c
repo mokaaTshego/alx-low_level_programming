f5b4dfcf8594276be4d6adc58912a56dfe607fb1
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-12 02:31:08 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen :  returns the length of a string
+ * @s : input string
+ * Return : length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
