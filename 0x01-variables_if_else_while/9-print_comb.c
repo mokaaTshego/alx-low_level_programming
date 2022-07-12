daca81112f50189f90f8143e4ccd8ec9c0d2adb8
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-12 15:33:08 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main : main code
+ * main : main block for printing combinations
  * Description : prints all possible combinations of single-digit numbers
  *  Return: 0
  */
