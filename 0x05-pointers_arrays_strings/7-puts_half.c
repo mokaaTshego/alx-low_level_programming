a6477b2b453b5822d8594b175ff0a5055c46cff2
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-12 06:00:38 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * puts_half - prints half of a string
- * @str - input string
+ * @str : input string
  * Return: Always 0.
  */
 void puts_half(char *str)
@@ -11,21 +11,20 @@ void puts_half(char *str)
 
 	len = strlen(str);
 
-	if ( len % 3 == 0)
+	if (len % 3 == 0)
 	{
-		len1 = (len -1) / 2;
-		for (i = len1 ; i <= len; i++)
+		len1 = (len - 1) / 2;
+		for (i = len1 + 1; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = (len / 2) - 1; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
 }
-	
