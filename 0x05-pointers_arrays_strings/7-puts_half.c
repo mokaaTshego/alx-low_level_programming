6197a70ba01afcb16b81aa9aa7c9a9ab44b6b2fa
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-12 05:53:22 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str - input string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, len, len1;
+
+	len = strlen(str);
+
+	if ( len % 3 == 0)
+	{
+		len1 = (len -1) / 2;
+		for (i = len1 ; i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = len / 2; i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+	
