b0ca1d71d86e56420b8fb89f366dc88a65862bcf
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-15 13:04:02 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * main - main code
- * Description: prints all single digit numbers of base 10
- * Return : always 0.
+ * Description: prints all single digit numbers of base ten
+ * Return: always 0. (success)
  */
 int main(void)
 {
