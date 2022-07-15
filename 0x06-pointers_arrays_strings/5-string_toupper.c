c6a7255cac23fe56e016eace92b2500e3b6d2781
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-15 08:11:25 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *string_toupper -  changes letters of a string to uppercase
+ * *string_toupper - change string to uppercase
  * @str : input string
  * Return : Always 0.
  */
