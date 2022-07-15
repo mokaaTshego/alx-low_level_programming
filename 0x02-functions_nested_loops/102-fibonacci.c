83544aff6b9a7c361394191feca309c74ebfed6b
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-15 15:11:55 -0700

    task 12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - main code
+ * Descrption :prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Returrn : Always 0
+ */
+int main(void)
+{
+	long int a, b, c, d;
+
+	b = 1;
+	a = 1;
+	c = 2;
+	while (a < 51)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+		a++;
+	}
+	return (0);
+}
