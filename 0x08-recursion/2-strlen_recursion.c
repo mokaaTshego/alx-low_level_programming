2e69ce0759ef72eaeeaa0a2d93ef13f9fd3795a4
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-21 12:26:48 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _strlen_recursion - returns the length of a string 
+ * _strlen_recursion - returns the length of a string
  * @s : input string pointer
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
 {
-	int count;
+	int count, i;
 
 	count = 0;
 	if (*s)
 	{
-		count += _strlen_recursion(s + 1) + 1;;
+		i = _strlen_recursion(s + 1);
+		count += (i + 1);
 	}
 	return (count);
 }
