a07b308f741f54c26648206f09d554fc471fd03e
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-26 13:24:52 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,12 +9,37 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i;
+	int i, j;
 
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
 	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			{
+				free(ptr[i]);
+			}
+			free(ptr[i]);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
 	}
 	return (ptr);
 }
