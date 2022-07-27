e2d6f5e7613a21069ec298e8711a2ec256ba9bfd
Tshegofatso Mokaa <desireemokaa1@gmail.com>
2022-07-27 07:59:39 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main - print program's name
  * @argc : size of array
  * @argv : array
- * Return : Always 0 
+ * Return: Always 0.
  */
 int main(int argc, char const *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
